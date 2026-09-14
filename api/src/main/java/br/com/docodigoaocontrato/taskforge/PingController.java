7bc0b5d875e96775b49fdfe1c3fdbe0acbb9ac00
Stéffany <fanybusta_mante@hotmail.com>
2026-09-14 20:20:04 -0300

    TaskforgeApplication e PingController.

---
+++ api/src/main/java/br/com/docodigoaocontrato/taskforge/PingController.java
@@ -0,0 +1,13 @@
+package br.com.docodigoaocontrato.taskforge;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
