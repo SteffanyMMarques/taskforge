5f4919bb7fbbc6c72d80a56adf27736f2e7a47e9
Stéffany <fanybusta_mante@hotmail.com>
2026-09-14 22:36:46 -0300

    Configuramdo h2 e mapeando tarefa.

---
+++ api/src/main/java/br/com/docodigoaocontrato/taskforge/model/Tarefa.java
@@ -0,0 +1,60 @@
+package br.com.docodigoaocontrato.taskforge.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private int prioridade;
+    private boolean concluida;
+
+    public Tarefa(){
+
+    }
+
+    public Tarefa(String nome, int prioridade, boolean concluida) {
+        this.nome = nome;
+        this.prioridade = prioridade;
+        this.concluida = concluida;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+}
